bec5483436071897882f7ae8361cfc557fe496af
Lohith097 <58364350+Lohith097@users.noreply.github.com>
2026-09-24 11:04:20 +0530

    Enhance output message in towerOfHanoi function

--- 3.cpp
+++ 3.cpp
@@ -3,9 +3,9 @@ using namespace std;
 
 void towerOfHanoi(int n, char source, char destination, char auxiliary) {
     if (n == 0) return;
-    
+
     towerOfHanoi(n - 1, source, auxiliary, destination);
-    cout << "Move disk from " << source << " to " << destination << "\n";
+    cout << "Move disk " << n << " from "<< source << " to " << destination << "\n";
     towerOfHanoi(n - 1, auxiliary, destination, source);
 }
 
